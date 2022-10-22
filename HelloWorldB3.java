f099b8f19f3be63f1de3ddf423e73eec5d013b61
devJamesFaulkner <116411746+devJamesFaulkner@users.noreply.github.com>
2022-10-21 21:10:20 -0400

    Add files via upload
    Hello World. First Java Program

---
+++ HelloWorldB3.java
@@ -0,0 +1,28 @@
+
+package helloworldb3;
+
+
+public class HelloWorldB3 {
+
+    public static void main(String[] args) { 
+      System.out.print("bob");
+      System.out.println("Hello World!");
+      System.out.println("Here's another line");
+      System.out.println("And another line to print");
+      System.out.println("Here's the last line");
+
+   //Ascii Art
+      System.out.println("_______________          |*\\_/*|________");
+      System.out.println("|  ___________  |        ||_/-\\_|______  |");
+      System.out.println("|  ___________  |        ||_/-\\\\_|______  |\")");
+      System.out.println("| |    0   0   | |     | |    0   0   | |");
+      System.out.println("| |   \\___/   | |        | |   \\___/   | |");
+      System.out.println("| |___    ___| |          | |___________| |");
+      System.out.println("|_____|\\_/|_____|        |_______________|");
+      System.out.println("|__|/ \\|_|_........      ......_|________|_");
+      System.out.println("/ ********** \\           / ********** \\");
+      System.out.println("/  ************ /    /  ************  \\");
+      System.out.println("-----------------   -----------------------");
+            }
+    
+}
